ea48f5f9ffc4e2c8a0d97a87ff9f70b43011195c
Henrique Nunes <79033028+riquen@users.noreply.github.com>
2024-12-02 19:39:37 -0300

    feat: colors

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,14 +1,24 @@
+'use client'
+
 import Box from "@src/components/Box/Box";
 import Background from "./patterns/Background/Background";
 import Menu from "./patterns/Menu/Menu";
 import Text from "@src/components/Text/Text";
 import Feed from "./patterns/Feed/Feed";
 import Footer from "./patterns/Footer/Footer";
+import { useTheme } from "@src/theme/ThemeProvider";
 
 export default function HomeScreen() {
+  const theme = useTheme();
+
   return (
     <Box
       tag="main"
+      styleSheet={{
+        backgroundColor: theme.colors.neutral.x400,
+        flex: 1,
+        alignItems: 'center',
+      }}
     >
       <Background />
       <Menu />
